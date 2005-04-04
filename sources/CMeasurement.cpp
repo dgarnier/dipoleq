86d6d0a82761fe6bb444116469341d2e65a42a7e
garnier <garnier>
2005-04-04 05:58:26 +0000

    Adding C++ style measurements...  This allows me to further understand how the code works and let me expand w/o missing important parts...

---
+++ sources/CMeasurement.cpp
@@ -0,0 +1,75 @@
+/*
+ *  CMeasurement.cpp
+ *  DipolEq
+ *
+ *  Created by Darren Garnier on 3/31/05.
+ *  Copyright 2005 Columbia University. All rights reserved.
+ *
+ */
+
+#include "CMeasurement.h"
+//#include "CMeasFluxLoop.h"
+//#include "CMeasBpCoil.h"
+//#include "CMeasCoilFlux.h"
+//#include "CMeasPressure.h"
+#include "nrutil.h"
+
+CMeasurement* CMeasurement::CreateWithType(char* mType) 
+{
+    CMeasurement *mp = NULL;
+    
+    
+    
+    return mp;
+}
+
+CMeasurement* CMeasurement::CreateWithType(int mType) 
+{
+    CMeasurement *mp = NULL;
+    
+    
+    
+    return mp;
+}
+
+
+void CMeasurement::SetParameter(char *key, char *val)
+{
+    const char * kInputWords[] = {
+        "Name",
+        "Value",
+        "StdDev"
+    };
+    
+    enum eInputCodes {
+        kName,
+        kValue,
+        kStdDev,
+        kDone
+    };
+    
+    int i;
+    
+    for (i=0; i<kDone; i++) 
+        if (!strcmp(kInputWords[i], key)) break;
+        
+    switch (i) {
+        case kValue:
+            sscanf(val,"%lf",&mValue);
+            break;
+        case kStdDev:
+            sscanf(val,"%lf",&mStdDev);
+            break;
+        case kName:
+            strncpy(mName,val,sizeof(mName)-1);
+            break;
+        default:
+        {
+            char err[256];
+            sprintf(err,"Measurement: Invalid key-value pair %s = %s\n",key,val);
+            nrerror(err);
+        }
+    }
+            
+    
+}
---
+++ sources/CMeasurement.h
@@ -0,0 +1,56 @@
+/*
+ *  CMeasurement.h
+ *  DipolEq
+ *
+ *  Created by Darren Garnier on 3/31/05.
+ *  Copyright 2005 Columbia University. All rights reserved.
+ *
+ *  Replaces the measurements.h types...
+ */
+
+#ifndef _CMeasurement_h_
+#define _CMeasurement_h_ 1
+
+#include <vector>
+
+class CMeasurement;
+
+typedef struct tokamak TOKAMAK;
+
+typedef std::vector<CMeasurement *> CMeasurementList;
+
+class CMeasurement {
+public:
+    double                  mValue;
+    double                  mStdDev;
+    double                  mFit;
+    double                  mNow;
+    char                    mName[32];
+
+    // virtual functions to find fits from measurements
+    virtual void            FindGreen(TOKAMAK *td) = 0;
+    virtual void            FindFit(TOKAMAK *td) = 0;
+    virtual void            FindNow(TOKAMAK *td) = 0;
+    virtual void            FindL(TOKAMAK *td, double *L) = 0;
+    
+    // read and create measurement from file!
+    static CMeasurement *   CreateWithType(char *);
+    static CMeasurement *   CreateWithType(int);
+    virtual void            SetParameter(char *, char *);
+    
+    static void             FindAllGreens(CMeasurementList &mlist);
+    static void             FindAllFit(CMeasurementList &mlist);
+    static void             FindAllNow(CMeasurementList &mlist);
+    static void             FindAllL(CMeasurementList &mlist);
+    static void             FreeAll(CMeasurementList &mlist);
+    
+    // destructor
+    virtual ~CMeasurement(void);
+    // virtual constructors!
+    virtual CMeasurement * create() const = 0; 
+    virtual CMeasurement * clone() const = 0;
+};
+
+#endif // _CMeasurement_h_
+
+
