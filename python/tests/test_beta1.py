14759e12e24b40bb876a2eb6d0824a6046ade48d
Benjy Smith <newzealandbenjy@gmail.com>
2025-04-08 14:23:36 +1200

    Fix ruff violations.

--- python/tests/test_beta1.py
+++ python/tests/test_beta1.py
@@ -6,8 +6,8 @@ from pathlib import Path
 from typing import Any
 
 import fixtures.typeguard_fix  # noqa: F401  # pylint: disable=unused-import
-import pytest
 import numpy as np
+import pytest
 from dipoleq import Machine
 from dipoleq.core import Machine as coreMachine
 from dipoleq.file_input import input_from_dotin
@@ -121,19 +121,21 @@ def test_dipoleq_is_cocos_11() -> None:
 
     # This value is the scale factor and sign in the Grad-Shafranov equation.
     # In COCOS terms, it should be equal to sigma_Bp * (2*pi)^e_Bp (which is +2pi for COCOS 11).
-    with np.errstate(divide='ignore', invalid='ignore'):
+    with np.errstate(divide="ignore", invalid="ignore"):
         grad_shafranov_scale = -current_grid / (np.array(m.PsiGrid.R) * pprime_grid)
 
     assert sign_delta_psi == +1  # COCOS 1,11,2,12,5,15,6,16
     assert np.all(sign_q == +1)  # COCOS 1,11,2,12,7,17,8,18
     violations = (
-        (~np.isclose(grad_shafranov_scale, +2*np.pi, rtol=0.1))
+        (~np.isclose(grad_shafranov_scale, +2 * np.pi, rtol=0.1))
         & np.isfinite(grad_shafranov_scale)
         & (grad_shafranov_scale != 0.0)
     )
     # Due to the residual not being precisely zero, there may be a few points
     # which are not equal to 2pi, but the vast majority (>99%) should be.
-    assert np.count_nonzero(violations) < 0.01 * grad_shafranov_scale.size # COCOS 11,12,15,16
+    assert (
+        np.count_nonzero(violations) < 0.01 * grad_shafranov_scale.size
+    ) # COCOS 11,12,15,16
 
 
 if __name__ == "__main__":
