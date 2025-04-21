1c663490abdcbd549a6a22cdf71976c81eba49c3
Benjy Smith <newzealandbenjy@gmail.com>
2025-04-22 08:30:11 +1200

    Put in time-dependent array functionality for OmasDS

--- python/dipoleq/ds.py
+++ python/dipoleq/ds.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from imas.ids_base import IDSBase
 from omas import ODS
+from typing import List
+import numpy as np
 
 
 class DS(ABC):
@@ -23,6 +25,8 @@ class DS(ABC):
             self[name] = value
     
     def _separate_key(self, key):
+        if isinstance(key, int):
+            return [key]
         split_key = key.replace('/', '.').replace(']', '').replace('[', '.').split('.')
         return [int(k) if k.isdecimal() else k for k in split_key]
     
@@ -32,6 +36,10 @@ class DS(ABC):
 
     @abstractmethod
     def _setitem(self, key, value):
+        ...
+
+    @abstractmethod
+    def __len__(self):
         ...
 
 
@@ -51,12 +59,31 @@ class ImasDS(DS):
 class OmasDS(DS):
     _ods: ODS
 
-    def _getitem(self, key) -> 'OmasDS':
+    def _getitem(self, key: List[str | int]) -> 'OmasDS':
         return self.__class__(self._ods[self._join_key(key)])
     
-    def _setitem(self, key, value):
-        self._ods[self._join_key(key)] = value
+    def _setitem(self, key: List[str | int], value):
+        assert len(key) > 0
+        if len(key) == 2 and isinstance(key[1], int):
+            # Deal with time-dependent arrays
+            orig_value = []
+            if key[0] in self._ods:
+                orig_value = np.atleast_1d(self._ods[key[0]]).toList()
+            if key[1] < len(orig_value):
+                orig_value[key[1]] = value
+            elif key[1] == len(orig_value):
+                orig_value += [value]
+            else:
+                raise IndexError
+            self._ods[key[0]] = np.atleast_1d(orig_value)
+        elif len(key) == 1:
+            self._ods[key[0]] = value
+        else:
+            OmasDS(self._ods[key[0]])._setitem(key[1:], value)
     
-    def _join_key(self, key):
+    def _join_key(self, key: List[str | int]) -> str:
         assert all(isinstance(k, str) or isinstance(k, int) for k in key)
         return '.'.join([str(k) for k in key])
+    
+    def __len__(self) -> int:
+        return len(self._ods)
