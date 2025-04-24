f2e9263a354d41832f2305527bfb9489eb38db4e
Benjy Smith <newzealandbenjy@gmail.com>
2025-04-24 13:16:59 +1200

    Add test for IMAS v3 vs v4

--- python/tests/test_imas.py
+++ python/tests/test_imas.py
@@ -3,6 +3,7 @@ import warnings
 from pathlib import Path
 
 from .fixtures import typeguard_fix  # noqa: F401  # pylint: disable=unused-import  # isort: skip
+import numpy as np
 import pytest
 from dipoleq import Machine
 try:
@@ -26,7 +27,19 @@ def test_imas_save(tmp_path_factory: pytest.TempPathFactory) -> Path:
     return fpath
 
 
-@pytest.mark.skipif("imas" not in globals(), reason="IMAS tests require IMAS-Python and dipmas")
+@pytest.fixture(scope="session")
+def test_imas_save_v4(tmp_path_factory: pytest.TempPathFactory) -> Path:
+    fpath = tmp_path_factory.mktemp("data") / "beta1_imas.nc"
+    m = Machine.from_file(data_dir / "beta1.yaml")
+    m.solve()
+    warnings.filterwarnings("ignore")
+    print(data_dir)
+    with DBEntry(str(fpath), mode="w", dd_version="4.0.0+dipole") as db:
+        m.to_imas(db)
+    return fpath
+
+
+@pytest.mark.skipif("DBEntry" not in globals(), reason="IMAS tests require IMAS-Python and dipmas")
 def test_imas_params(test_imas_save: Path) -> None:
     with DBEntry(test_imas_save, "r", dd_version="3.41.0+dipole") as db:
         assert db.get("equilibrium")["code/name"] == "DipolEQ"
@@ -37,3 +50,14 @@ def test_imas_params(test_imas_save: Path) -> None:
         m1 = Machine.from_dict(input_data)
         m0 = Machine.from_file(data_dir / "beta1.yaml")
         assert m0 == m1
+
+
+@pytest.mark.skipif("DBEntry" not in globals(), reason="IMAS tests require IMAS-Python and dipmas")
+def test_imas_v4(test_imas_save: Path, test_imas_save_v4: Path) -> None:
+    with DBEntry(test_imas_save, "r", dd_version="3.41.0+dipole") as db:
+        psi_v3 = np.array(db.get("equilibrium")["time_slice[0]/profiles_1d/psi"])
+    
+    with DBEntry(test_imas_save_v4, "r", dd_version="4.0.0+dipole") as db:
+        psi_v4 = np.array(db.get("equilibrium")["time_slice[0]/profiles_1d/psi"])
+    
+    np.testing.assert_allclose(psi_v3, -psi_v4)
